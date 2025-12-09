13ded1f77503d78d88aca2ea0e6d4887e51009e3
Wang, Yung-Hsiang <yh.richard.wang@gmail.com>
2025-12-08 20:29:19 -0800

    Update fragment.glsl

--- fragment.glsl
+++ fragment.glsl
@@ -4,7 +4,6 @@ precision highp float;
 uniform vec2 iResolution;
 uniform float iTime;
 uniform vec2 iMouse;
-// Drag multiplier removed from UI; use fixed constant here
 const float DRAG_MULT = 0.38;
 uniform float WATER_DEPTH;
 uniform float CAMERA_HEIGHT;
